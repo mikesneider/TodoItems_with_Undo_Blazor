e2a7b6a0bfb6a312511984fdd81274fbe4cb8f47
Mike <mikesneider@users.noreply.github.com>
2022-01-12 17:11:15 -0500

    Agregar archivos de proyecto.

---
+++ TodoItemsLearn/Pages/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoItemsLearn
+{
+    public class TodoItem
+    {
+        [Required]
+        public string Tittle { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
---
+++ TodoItemsLearn/Pages/TodoState.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoItemsLearn
+{
+    public class TodoState
+    {
+        public readonly Stack<IEnumerable<TodoItem>> TodoBuffer = new();
+        public IEnumerable<TodoItem> Current => TodoBuffer.Any() ? TodoBuffer.Peek() : new List<TodoItem>();
+        public List<TodoItem> todoItems { get; set; }
+
+        public bool CantUndo => TodoBuffer.Count > 0;
+        public bool CanClear => !Current.Any(i => i.IsDone);
+
+        public void addTodo(TodoItem newTodo)
+        {
+            var newState = new List<TodoItem>(Current) { newTodo };
+            //todoItems.Add(newTodo);
+            TodoBuffer.Push(newState);
+        }
+
+        public void ClearTodos()
+        {
+            //push
+            TodoBuffer.Push(Current.Where(i => !i.IsDone));
+            //todoItems = todoItems.Where(i => !i.IsDone).ToList();
+        }
+
+        public void Undo()
+        {
+            if(CantUndo)
+            {
+                TodoBuffer.Pop();
+            }
+             
+
+        }
+    }
+}
